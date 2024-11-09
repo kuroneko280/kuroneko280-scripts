ceccf01c208bab5f42ab94cd01f54aa6e9dfc935
kuroneko280 <kss2934@yahoo.co.jp>
2024-11-09 12:09:15 +0900

    Update niconico-embed.js

--- niconico-embed.js
+++ niconico-embed.js
@@ -2,5 +2,5 @@
 $('.niconico').click(function() {
     var videoURL = $(this).attr('data-video');
     var iframe = '<iframe src="' + videoURL + '" width="800" height="480" frameborder="0" allowfullscreen></iframe>';
-    $(this).html(iframe); // サムネイルをiframeに置き換える
+    $(this).html(iframe);
 });
